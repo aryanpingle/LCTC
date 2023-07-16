323df173d85a215eba8dfdffd39cffbf7a5defd8
robo-mop <realaryanpingle@gmail.com>
2023-07-17 01:15:29 +0530

    Fix run-build

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import serve from "rollup-plugin-serve";
 // import css from 'rollup-plugin-import-css'
 import postcss from 'rollup-plugin-postcss';
 
+const IS_PRODUCTION = !process.env.ROLLUP_WATCH;
+
 export default {
     input: 'src/App/index.tsx',
     output: {
@@ -18,7 +20,7 @@ export default {
             extract: true,
             modules: true,
         }),
-        serve({
+        !IS_PRODUCTION && serve({
             port: 5500,
             contentBase: 'build'
         })
