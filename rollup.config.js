b28f5e7897fae656db24aa37575495638c4b37b5
robo-mop <realaryanpingle@gmail.com>
2023-07-17 01:44:41 +0530

    Actually create an index.html

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,13 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import serve from "rollup-plugin-serve";
-// import css from 'rollup-plugin-import-css'
 import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
+// import { rimrafSync } from 'rimraf';
 
 const IS_PRODUCTION = !process.env.ROLLUP_WATCH;
+
+// rimrafSync("build/")
 
 export default {
     input: 'src/App/index.tsx',
@@ -19,6 +22,11 @@ export default {
         postcss({
             extract: true,
             modules: true,
+        }),
+        copy({
+            targets: [
+                { src: "src/root/*", dest: "build/" }
+            ]
         }),
         !IS_PRODUCTION && serve({
             port: 5500,
